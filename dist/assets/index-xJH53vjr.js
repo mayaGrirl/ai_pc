b6297308ff0f486605972e79ce738317c4ab463c
Tom <>
2026-01-31 22:21:16 +0000

    build

---
+++ dist/assets/index-xJH53vjr.js
@@ -0,0 +1,1 @@
+import{a2 as a,a3 as i}from"./index-BHIK7C10.js";import"./MainLayout-B5rIQJ09.js";const r=a();r.use(i);
---
+++ dist/assets/message-square-BG3iNLiZ.js
@@ -0,0 +1,11 @@
+import{c as a}from"./MainLayout-B5rIQJ09.js";/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=a("ArrowRightIcon",[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"m12 5 7 7-7 7",key:"xquz4c"}]]);/**
+ * @license lucide-vue-next v0.468.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=a("MessageSquareIcon",[["path",{d:"M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z",key:"1lielz"}]]);export{o as A,r as M};
